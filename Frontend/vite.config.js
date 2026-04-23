73de63acdb52e488617e191e48fec49d7a144cb1
Antoine Estienne <estienne.antoine@gmail.com>
2026-04-23 18:58:48 +0200

    tsconfig.node.tsbuildinfo and vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.wasm"],
-  define: {
-    global: "globalThis",
-  },
-  optimizeDeps: {
-    exclude: ["@zama-fhe/relayer-sdk"],
-    esbuildOptions: {
-      define: {
+    plugins: [react()],
+    assetsInclude: ["**/*.wasm"],
+    define: {
         global: "globalThis",
-      },
     },
-  },
-  server: {
-    port: 5173,
-  },
+    optimizeDeps: {
+        exclude: ["@zama-fhe/relayer-sdk"],
+        esbuildOptions: {
+            define: {
+                global: "globalThis",
+            },
+        },
+    },
+    server: {
+        port: 5173,
+    },
 });
